b10fb017100a19e8f99f49dde9d59304224d3471
Endermaiter <mpereznovoa@danielcastelao.org>
2023-03-09 12:46:34 +0100

    Error ejercicio 2

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import com.mongodb.client.*;
+import org.bson.Document;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class Main {
+
+    static MongoClient mongoClient = MongoClients.create();
+    static MongoDatabase database = mongoClient.getDatabase("test");
+    static MongoCollection<Document> collection = database.getCollection("vendas");
+
+    @SuppressWarnings("JpaQlInspection")
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("vehicli.odb");
+        EntityManager em = emf.createEntityManager();
+
+        //AMOSAR DATOS DA COLECCION VENDAS
+
+        MongoCursor<Document> cursor = collection.find().iterator();
+
+        while (cursor.hasNext()) {
+            Document doc = cursor.next();
+            int id = doc.getInteger("_id");
+            String dni = doc.getString("dni");
+            String codveh = doc.getString("codveh");
+            System.out.println(id + " ," + dni + " ," + codveh);
+
+            //AMOSAR O NOME E O NUMERO DE COMPRAS DO CLIENTE CORRESPONDIENTE
+            TypedQuery<Clientes> query =
+                    em.createQuery("SELECT c FROM Clientes c WHERE c.dni = '" + dni + "'", Clientes.class);
+            Clientes c = query.getSingleResult();
+            System.out.println("nomec -> " + c.nomec);
+        }
+    }
+}
